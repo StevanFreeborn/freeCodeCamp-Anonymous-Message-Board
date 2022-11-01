e4ebb330378962e441226b3cc23bca9c57ca0633
StevanFreeborn <sfreeborn7@gmail.com>
2022-11-01 10:28:08 -0500

    added nodemon. stubbed out functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,43 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function () {
+    test('Can create a new thread', done => {
 
+    });
+
+    test('Can view 10 most recent threads with 3 replies each', done => {
+
+    });
+
+    test('Can not delete a thread with incorrect password', done => {
+
+    });
+
+    test('Can delete a thread with correct password', done => {
+
+    });
+
+    test('Can report a thread', done => {
+
+    });
+
+    test('Can create a reply to a thread', done => {
+
+    });
+
+    test('Can view a thread and all its replies', done => {
+
+    });
+
+    test('Can not delete a reply with inccorect password', done => {
+
+    });
+
+    test('Can delete a reply with correct password', done => {
+
+    });
+
+    test('Can report a reply', done => {
+        
+    });
 });
