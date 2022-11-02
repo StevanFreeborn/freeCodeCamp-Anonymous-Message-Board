e009791a770f14d7fd4e0870b8a4fb58fd336317
StevanFreeborn <sfreeborn7@gmail.com>
2022-11-02 06:56:20 -0500

    moved express setup and app startup to separate files

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 'use strict';
 require('dotenv').config();
-const setupExpress = require('./middleware/setupExpress.js');
+const setupExpress = require('./startup/setupExpress.js');
 const helmetMiddleware = require('./middleware/helmetMiddleware.js');
 const apiRoutes = require('./routes/api.js');
 const appRoutes = require('./routes/app.js');
 const fccTestingRoutes = require('./routes/fcctesting.js');
 const runner = require('./test-runner');
 const notFoundMiddleware = require('./middleware/notFoundMiddleware.js');
+const startApp = require('./startup/startApp.js');
 
 const app = setupExpress();
 
@@ -15,20 +16,6 @@ fccTestingRoutes(app);
 apiRoutes(app);
 appRoutes(app);
 notFoundMiddleware(app);
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port);
-  if (process.env.NODE_ENV === 'test') {
-    console.log('Running Tests...');
-    setTimeout(() => {
-      try {
-        runner.run();
-      } catch (e) {
-        console.log('Tests are not valid:');
-        console.error(e);
-      }
-    }, 1500);
-  } 
-});
+startApp(app);
 
 module.exports = app;
---
+++ startup/startApp.js
@@ -0,0 +1,16 @@
+module.exports = function (app) {
+    const listener = app.listen(process.env.PORT || 3000, () => {
+        console.log('Your app is listening on port ' + listener.address().port);
+        if (process.env.NODE_ENV === 'test') {
+            console.log('Running Tests...');
+            setTimeout(() => {
+                try {
+                    runner.run();
+                } catch (e) {
+                    console.log('Tests are not valid:');
+                    console.error(e);
+                }
+            }, 1500);
+        }
+    });
+}
