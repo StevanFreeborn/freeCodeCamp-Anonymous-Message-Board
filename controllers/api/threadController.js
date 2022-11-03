51c8d8cf666a7ce1c7ceaa77ce1b68f80eed9ad0
Stevan Freeborn <sfreeborn7@gmail.com>
2022-11-03 16:26:11 -0500

    begin working on implementing creating a thread when given a particular board name

---
+++ controllers/api/threadController.js
@@ -0,0 +1,7 @@
+class ThreadController {
+    createThread = async (req, res) => {
+        const { board } = req.params;
+    }
+}
+
+module.exports = ThreadController;
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,9 @@
 'use strict';
+const ThreadController = require('../controllers/api/threadController.js');
+const threadController = new ThreadController();
 
 module.exports = function (app) {
+  app.post('/api/threads/:board', threadController.createThread);
   app.get('/api/threads/:board');
   app.get('/api/replies/:board');
 };
