322e7e0463151d8ab690f5c30833441b7f3c74e6
StevanFreeborn <sfreeborn7@gmail.com>
2022-11-02 20:05:18 -0500

    added id and name properties to board schema

--- models/board.js
+++ models/board.js
@@ -4,7 +4,10 @@ const boardSchemaOptions = {
     timestamps: true,
 }
 
-const BoardSchema = mongoose.Schema({}, boardSchemaOptions);
+const BoardSchema = mongoose.Schema({
+    id: { type: mongoose.Types.ObjectId },
+    name: { type: String, trim: true, }
+}, boardSchemaOptions);
 
 const Board = mongoose.model('boards', BoardSchema);
 
