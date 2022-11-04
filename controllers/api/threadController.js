6d3caad339a3beb0454741efd67844366c99fc75
Stevan Freeborn <sfreeborn7@gmail.com>
2022-11-04 08:24:07 -0500

    add bcrypt for hashing delete passwords before storing them

--- controllers/api/threadController.js
+++ controllers/api/threadController.js
@@ -4,13 +4,17 @@ const boardService = new BoardService();
 class ThreadController {
     createThread = async (req, res) => {
         const boardName = req.params.board;
+        const { text, delete_password, } = req.body;
+
         let board = await boardService.getBoardByName(boardName);
 
         if (board == null) {
             board = await boardService.createBoard(boardName);
         }
 
-        return res.status(201).json({ board });
+        
+
+        return res.status(201).json({ text, delete_password, });
     }
 }
 
