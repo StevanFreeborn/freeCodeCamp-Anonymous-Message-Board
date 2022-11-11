bf5bc0f75273f35878fcf985834db7bff333083a
Stevan Freeborn <sfreeborn7@gmail.com>
2022-11-10 23:03:03 -0600

    feat: wired up new add thread form on playground page. add highlight.js solarized theme.

--- public/scripts/playground.js
+++ public/scripts/playground.js
@@ -1,82 +1,111 @@
 $(function () {
-    $('#add-thread-form').submit(e => {
-        e.preventDefault();
-        const data = formDataToJson($(e.target).serializeArray());
-        console.log(data);
+  $("#add-thread-form").submit(addThread);
 
-        // $.ajax({
-        //     type: 'POST',
-        //     url: `/api/threads/${boardName}`,
-        //     data: { name: boardName, },
-        //     success: (board) => {
-        //         $('#add-board-form')[0].reset();
-        //         addBoardElement(board);
-        //     },
-        //     error: (res, err) => {
-        //         const error = res.responseJSON?.error ?? 'Failed to add board';
-        //         $('#add-board-error').text(error)
-        //     },
-        // });
-    });
+  $("#add-thread-board").on("input", (e) => {
+    $("#add-thread-board-error").text("");
+  });
 
-    $('#newReply').submit(function () {
-        var board = $('#board4').val();
-        $(this).attr('action', "/api/replies/" + board);
-    });
+  $("#add-thread-text").on("input", (e) => {
+    $("#add-thread-text-error").text("");
+  });
 
-    $('#reportThread').submit(function (e) {
-        var url = "/api/threads/" + $('#board2').val();
-        $.ajax({
-            type: "PUT",
-            url: url,
-            data: $(this).serialize(),
-            success: function (data) {
-                alert(data);
-            }
-        });
-        e.preventDefault();
+  $("#add-thread-delete-password").on("input", (e) => {
+    $("#add-thread-delete-password-error").text("");
+  });
+
+  $("#newReply").submit(function () {
+    var board = $("#board4").val();
+    $(this).attr("action", "/api/replies/" + board);
+  });
+
+  $("#reportThread").submit(function (e) {
+    var url = "/api/threads/" + $("#board2").val();
+    $.ajax({
+      type: "PUT",
+      url: url,
+      data: $(this).serialize(),
+      success: function (data) {
+        alert(data);
+      },
     });
-    $('#deleteThread').submit(function (e) {
-        var url = "/api/threads/" + $('#board3').val();
-        $.ajax({
-            type: "DELETE",
-            url: url,
-            data: $(this).serialize(),
-            success: function (data) {
-                alert(data);
-            }
-        });
-        e.preventDefault();
+    e.preventDefault();
+  });
+  $("#deleteThread").submit(function (e) {
+    var url = "/api/threads/" + $("#board3").val();
+    $.ajax({
+      type: "DELETE",
+      url: url,
+      data: $(this).serialize(),
+      success: function (data) {
+        alert(data);
+      },
     });
-    $('#reportReply').submit(function (e) {
-        var url = "/api/replies/" + $('#board5').val();
-        $.ajax({
-            type: "PUT",
-            url: url,
-            data: $(this).serialize(),
-            success: function (data) {
-                alert(data);
-            }
-        });
-        e.preventDefault();
+    e.preventDefault();
+  });
+  $("#reportReply").submit(function (e) {
+    var url = "/api/replies/" + $("#board5").val();
+    $.ajax({
+      type: "PUT",
+      url: url,
+      data: $(this).serialize(),
+      success: function (data) {
+        alert(data);
+      },
     });
-    $('#deleteReply').submit(function (e) {
-        var url = "/api/replies/" + $('#board6').val();
-        $.ajax({
-            type: "DELETE",
-            url: url,
-            data: $(this).serialize(),
-            success: function (data) {
-                alert(data);
-            }
-        });
-        e.preventDefault();
+    e.preventDefault();
+  });
+  $("#deleteReply").submit(function (e) {
+    var url = "/api/replies/" + $("#board6").val();
+    $.ajax({
+      type: "DELETE",
+      url: url,
+      data: $(this).serialize(),
+      success: function (data) {
+        alert(data);
+      },
     });
+    e.preventDefault();
+  });
 });
 
+const addThread = (e) => {
+  e.preventDefault();
+  const data = formDataToJson($(e.target).serializeArray());
+  const hasBoard = data?.board ? true : false;
+  const hasText = data?.text ? true : false;
+  const hasDeletePassword = data?.delete_password ? true : false;
+
+  if (!hasBoard || !hasText || !hasDeletePassword) {
+    if (!hasBoard) $("#add-thread-board-error").text("Please select a board");
+    if (!hasText) $("#add-thread-text-error").text("Please enter thread text");
+    if (!hasDeletePassword)
+      $("#add-thread-delete-password-error").text(
+        "Please enter a delete password"
+      );
+    return;
+  }
+
+  $.ajax({
+    type: "POST",
+    url: `/api/threads/${data.board}`,
+    data: data,
+    success: (thread) => {
+      displayResponse($("#new-thread-response"), thread);
+    },
+    error: (res, err) => {
+      displayResponse($("#new-thread-response"), res.responseJSON);
+    },
+  });
+};
+
 const formDataToJson = (formData) => {
-    return formData.reduce((prev, curr) => {
-        prev[curr.name] = curr.value;
-        return prev;
-    }, {});
-}
+  return formData.reduce((prev, curr) => {
+    prev[curr.name] = curr.value;
+    return prev;
+  }, {});
+};
+
+const displayResponse = (jObj, response) => {
+  jObj.text(JSON.stringify(response, null, 4));
+  hljs.highlightAll();
+};
