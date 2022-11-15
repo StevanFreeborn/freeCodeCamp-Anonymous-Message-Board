51cbff888ea7a2e79befe3f06d2a558b806e822f
Stevan Freeborn <sfreeborn7@gmail.com>
2022-11-15 17:26:31 -0600

    fix: clean up add board handler into separate function

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,28 +3,7 @@ import createBoardCard from './components/boardCard.js';
 $(async () => {
   await displayBoards();
 
-  $('#add-board-form').submit(e => {
-    e.preventDefault();
-
-    const boardName = $('#board-name').val();
-
-    if (!boardName)
-      return $('#add-board-error').text('Please provide a name for the board');
-
-    $.ajax({
-      type: 'POST',
-      url: '/api/boards',
-      data: { name: boardName },
-      success: board => {
-        $('#add-board-form')[0].reset();
-        addBoard(board);
-      },
-      error: (res, err) => {
-        const error = res.responseJSON?.error ?? 'Failed to add board';
-        $('#add-board-error').text(error);
-      },
-    });
-  });
+  $('#add-board-form').submit(addBoard);
 
   $('#board-name').on('input', e => {
     $('#add-board-error').text('');
@@ -39,7 +18,30 @@ const displayBoards = async () => {
   });
 };
 
-const addBoard = board => {
+const addBoard = e => {
+  e.preventDefault();
+
+  const boardName = $('#board-name').val();
+
+  if (!boardName)
+    return $('#add-board-error').text('Please provide a name for the board');
+
+  $.ajax({
+    type: 'POST',
+    url: '/api/boards',
+    data: { name: boardName },
+    success: board => {
+      $('#add-board-form')[0].reset();
+      addBoardCard(board);
+    },
+    error: (res, err) => {
+      const error = res.responseJSON?.error ?? 'Failed to add board';
+      $('#add-board-error').text(error);
+    },
+  });
+};
+
+const addBoardCard = board => {
   const boardCard = createBoardCard(board);
   $('#boards-container').prepend(boardCard);
 };
